d7b58d6880eecf7e3c3bf4a49684266877f31ada
admegor <admegor@mail.ru>
2020-08-19 20:33:54 +0500

    3 screens step 5

--- src/js/slick-slider.js
+++ src/js/slick-slider.js
@@ -3,23 +3,24 @@ $(document).ready(function() {
     $('.services__slider').slick({
         arrows: true,
         slidesToShow: 3,        
-        slidesToScroll: 3,
-//         responsive: [
-//                 {
-//           breakpoint: 975,
-//           settings: {
-//             slidesToShow: 2,
-//             slidesToScroll: 2
-//           }
-//         },
-//         {
-//           breakpoint: 640,
-//           settings: {
-//             slidesToShow: 1,
-//             slidesToScroll: 1
-//           }
-//         }
-//   ]
+        slidesToScroll: 1,
+        infinite: false,
+        responsive: [
+                {
+          breakpoint: 890,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 540,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+  ]
         
     });
 });
