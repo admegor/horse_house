8a2b74656552e66a31d05b8269784a1a43fe1d8f
Egor Yurchenko <45126330+admegor@users.noreply.github.com>
2020-09-16 22:45:39 +0500

    menu3

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,14 +3,28 @@ let menuNav = document.querySelector('.header__nav-wrapper');
 let menuList = document.querySelector('.header__nav');
 let menuBurger = document.querySelector('.header__menu-burger');
 let bodySite = document.body;
+let header = document.querySelector('.header');
+
+const lockPadding = document.querySelectorAll(".lock-padding");
+const lockPaddingValue = window.innerWidth - document.querySelector('.header').offsetWidth + 'px';
+
 
 
 menuBtn.onclick = function() {
-    menuNav.classList.toggle('header__nav-wrapper--show');
+    menuNav.classList.toggle('open');
     menuBurger.classList.toggle('active');
     menuList.classList.toggle('active');
     bodySite.classList.toggle('lock');
-    const lockPaddingValue = window.innerWidth - document.querySelector('.header').offsetWidth + 'px';
-    
     bodySite.style.paddingRight = lockPaddingValue;
+    header.style.paddingRight = lockPaddingValue;
+
+
+    if(!bodySite.classList.contains("lock")) {
+      
+		bodySite.style.paddingRight = '0px';
+		header.style.paddingRight = '0px';
+      
+	}
 };
+
+
