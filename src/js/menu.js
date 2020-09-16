d19b52d7f08674b051736025a39bc04687d9bef3
admegor <admegor@mail.ru>
2020-09-16 15:54:04 +0500

    menu2

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,9 +4,13 @@ let menuList = document.querySelector('.header__nav');
 let menuBurger = document.querySelector('.header__menu-burger');
 let bodySite = document.body;
 
+
 menuBtn.onclick = function() {
     menuNav.classList.toggle('header__nav-wrapper--show');
     menuBurger.classList.toggle('active');
     menuList.classList.toggle('active');
     bodySite.classList.toggle('lock');
+    const lockPaddingValue = window.innerWidth - document.querySelector('.header').offsetWidth + 'px';
+    
+    bodySite.style.paddingRight = lockPaddingValue;
 };
